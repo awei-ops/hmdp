869a0e923087a7a74a020043dc265c88b1da67cf
awei-ops <1067181221@qq.com>
2026-04-23 18:04:26 +0800

    商品redis查询实现，String类型

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,9 +1,14 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +21,23 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
-
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result queryById(Long id) {
+        //从redis里查询
+        String s = stringRedisTemplate.opsForValue().get("cash:shop:"+id);
+        //判断是否cunzai
+        if (StrUtil.isNotBlank(s)) {
+            //存在，直接返回
+            return Result.ok(s);
+        }
+        //不存在，根据id查询数据库
+        Shop shop = getById(id);
+        if(shop==null){
+            return Result.fail("数据不存在");
+        }
+        stringRedisTemplate.opsForValue().set("cash:shop:"+id, JSONUtil.toJsonStr(shop));
+        return Result.ok(shop);
+    }
 }
